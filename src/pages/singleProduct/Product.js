8050fbba8f91699aa3175bf7a5777eb8002bf1b4
alireza139 <alireza.hosseinii66@gmail.com>
2025-09-15 20:21:02 +0330

    Add Error40

--- src/pages/singleProduct/Product.js
+++ src/pages/singleProduct/Product.js
@@ -3,26 +3,27 @@ import "./Product.css"
 import Chart from "../../components/chart/Chart"
 import { useParams, Navigate } from 'react-router-dom'
 import { productsInfo } from '../../CmsDB'
-import Table from 'react-bootstrap/Table';
+
 
 export default function Product() {
-    let [productsInf, setProductsInf] = useState(productsInfo)
+    let [productsInf] = useState(productsInfo)
     let params = useParams()
+
     // یافتن محصول تکی موردنظر
     let mainProduct = productsInf.find(p => p.id == params.productID)
+
+    // اگر محصول پیدا نشد → برو به صفحه 404
+    if (!mainProduct) {
+        return <Navigate to="../error404/Error404" replace />
+    }
+
     // کل فروش محصول
-    let allSell = 0
-    mainProduct.saleInfo.forEach(item => allSell += item.sale)
-
-
-
-
-
+    let allSell = mainProduct.saleInfo.reduce((acc, item) => acc + item.sale, 0)
 
     return (
         <div className='product-page'>
             <div className="leftSection">
-                <Chart grid title="Sale Chart" data={mainProduct.saleInfo} datakey="sale"></Chart>
+                <Chart grid title="Sale Chart" data={mainProduct.saleInfo} datakey="sale" />
             </div>
             <div className="rightSection">
                 <img src={mainProduct.imgSrc} alt="product image" className='product-image' />
@@ -48,6 +49,5 @@ export default function Product() {
                 </div>
             </div>
         </div>
-
     )
 }
--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,17 @@ import Users from './pages/users/Users'
 import NewUser from './pages/newUser/NewUser'
 import ProductsList from './pages/productsList/ProductsList'
 import Product from './pages/singleProduct/Product'
+import Error404 from './pages/error404/Error404'
+
 
 const routes = [
     { path: '/', element: <Home></Home> },
     { path: '/users', element: <Users></Users> },
     { path: '/newUser', element: <NewUser></NewUser> },
     { path: '/products', element: <ProductsList></ProductsList> },
-    { path: '/product/:productID', element: <Product></Product> },
-    
+    { path: 'product/:productID', element: <Product></Product> },
+
+    // باید آخرین روت باشه
+    { path: '*', element: <Error404 /> },
 ]
 export default routes
